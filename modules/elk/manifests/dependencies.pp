f134d54961045c1a18f192a6dba8f18d83f5a71f
AAlvz <alfonso@alvz.mx>
2015-01-16 20:21:51 +0100

    of course emacs is a dependecy

--- modules/elk/manifests/dependencies.pp
+++ modules/elk/manifests/dependencies.pp
@@ -1,12 +1,16 @@
 class elk::dependencies{
   exec {'update':
-    user    => root,
-    command => 'sudo apt-get update -y --fix-missing',
-    provider=> "shell",
-    path => ["/bin", "/usr/bin", "/usr/sbin"],
+    user     => root,
+    command  => 'sudo apt-get update -y --fix-missing',
+    provider => "shell",
+    path     => ["/bin", "/usr/bin", "/usr/sbin"],
   } ->
 
   package {'openjdk-7-jdk':
+    ensure => present,
+  }
+
+  package {'emacs':
     ensure => present,
   }
 }
