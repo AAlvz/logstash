6fbeefdf775ef11eb37b270c6f62889024a9ee6e
AAlvz <alfonso@alvz.mx>
2015-02-19 15:48:44 +0100

    Testing new logs

--- modules/elk/manifests/params.pp
+++ modules/elk/manifests/params.pp
@@ -3,5 +3,5 @@ class elk::params{
   $shell    = '/bin/bash'
   $password = '$6$WlOB9RFQAZ99qwC$gU9G6wnL.H7HMm3.zJq2c5ITqCGqxFhk8y9kNuF9HBkLc7JTYsa2TAcu1oOifw5jG/tHFPVTyeZixcWYQVRpL.'
   $comment  = 'Created by Puppet'
-  $log_path     = '/home/aalvarez/logstash/logs/millionlines'
+  $log_path     = '/var/log/abp/media.log'
 }
