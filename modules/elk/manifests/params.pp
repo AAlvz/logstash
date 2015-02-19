bdce10ebad961142dedb04af639d6e5d33635a6d
AAlvz <alfonso@alvz.mx>
2015-02-19 17:09:49 +0100

    setting params for vagrant test

--- modules/elk/manifests/params.pp
+++ modules/elk/manifests/params.pp
@@ -1,5 +1,5 @@
 class elk::params{
-  $user     = 'aalvarez'
+  $user     = 'user'
   $shell    = '/bin/bash'
   $password = '$6$WlOB9RFQAZ99qwC$gU9G6wnL.H7HMm3.zJq2c5ITqCGqxFhk8y9kNuF9HBkLc7JTYsa2TAcu1oOifw5jG/tHFPVTyeZixcWYQVRpL.'
   $comment  = 'Created by Puppet'
